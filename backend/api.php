9d712201a784b58b120a4ae09363f058af3eebec
Daniel <dana.kochergin99@gmail.com>
2023-12-29 01:51:08 +0400

    first commit

--- backend/api.php
+++ backend/api.php
@@ -1,1 +1,35 @@
 <?php
+class AmoCRM {
+    private $domain;
+    private $clientId;
+    private $clientSecret;
+    private $redirectUri;
+    private $code;
+    private $price;
+
+    public function __construct($domain, $clientId, $clientSecret, $redirectUri, $code, $price) {
+        $this->domain = $domain;
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+        $this->redirectUri = $redirectUri;
+        $this->code = $code;
+        $this->price = $price;
+    }
+
+    public function createContact($name, $email, $phone, $price) {
+        // Здесь нужно реализовать создание контакта в AmoCRM
+    }
+
+    public function createLead($name, $price, $contactId) {
+        // Здесь нужно реализовать создание сделки в AmoCRM
+    }
+}
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$amoCRM = new AmoCRM('localhost', 'your-client-id',  'your-redirect-uri', 'your-code');
+
+$contact = $amoCRM->createContact($data['name'], $data['email'], $data['phone']);
+$lead = $amoCRM->createLead($data['name'], $data['price'], $contact['id']);
+
+echo json_encode(['contact' => $contact, 'lead' => $lead]);
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form id="form" @submit.prevent="submitForm">
+      <label>
+        <input type="text" v-model="name" placeholder="Имя:" required>
+      </label>
+      <label>
+        <input type="email" v-model="email" placeholder="Email:" required>
+      </label>
+      <label>
+        <input type="tel" v-model="phone" placeholder="Телефон:" required>
+      </label>
+      <label>
+        <input type="number" v-model="price" placeholder="Цена:" required>
+      </label>
+      <button type="submit">Отправить</button>
+    </form>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      price: '',
+      phone: '',
+      email: '',
+      name: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      const data = {
+        name: this.name,
+        email: this.email,
+        phone: this.phone,
+        price: this.price
+      };
+      axios.post('/api/create-lead.php', data)
+          .then((res) => {
+            console.log(res)
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+    }
   }
+
+
 }
 </script>
 
@@ -19,8 +58,46 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  display: flex;
+  justify-content: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#form {
+  display: flex;
+  align-items: center;
+  row-gap: 20px;
+  flex-direction: column;
+  width: 500px;
+}
+label{
+  width: 500px;
+}
+input {
+  width: 100%;
+  height: 40px;
+  padding:10px;
+  margin:10px 0; // add top and bottom margin
+}
+button {
+  /* remove default behavior */
+  appearance:none;
+  -webkit-appearance:none;
+  /* usual styles */
+  padding:10px;
+  border:none;
+  background-color:#3F51B5;
+  color:#fff;
+  font-weight:600;
+  border-radius:5px;
+  width:250px;
+}
+
+input {
+  padding:10px;
+  border:0;
+  box-shadow:0 0 15px 4px rgba(0,0,0,0.06);
+}
+
 </style>
